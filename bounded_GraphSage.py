9497c2877596353408bfbe0c1a3dceff3bc2a213
JasmeetSinghIITD <65686608+JasmeetSinghIITD@users.noreply.github.com>
2023-04-05 17:24:58 +0530

    Update bounded_GraphSage.py

--- bounded_GraphSage.py
+++ bounded_GraphSage.py
@@ -141,35 +141,21 @@ class BoundedGraphSAGE(nn.Module):
         self.adj_norm = None
         self.features = None
     def preprocess_adj(adj, device):
-        """
-        Preprocessing of adjacency matrix for GraphSage.
-
-    Parameters
-    ----------
-     adj : scipy.sparse.csr_matrix or torch.Tensor
-        The adjacency matrix of the graph.
-    device : str
-        'cpu' or 'cuda'.
-
-    Returns
-    -------
-    adj_norm : torch.Tensor
-        The normalized adjacency matrix of the graph.
-        """
+    
         adj = adj.to(device)
 
-        # Add self-loops to adjacency matrix
+    
         adj = adj + torch.eye(adj.shape[0], device=device)
 
-        # Compute the degree matrix
+    
         deg = torch.sparse.sum(adj, dim=1).to_dense()
         deg_inv_sqrt = deg.pow(-0.5)
         deg_inv_sqrt[torch.isinf(deg_inv_sqrt)] = 0.
 
-        # Compute the normalized adjacency matrix
         adj_norm = deg_inv_sqrt.unsqueeze(1) * adj * deg_inv_sqrt.unsqueeze(0)
-    
+
         return adj_norm
+
 
 	
     def embed(self, x, adj):
