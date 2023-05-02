29678d0da6471ceb57bfcc6cebd3b57f308d25b5
JasmeetSinghIITD <65686608+JasmeetSinghIITD@users.noreply.github.com>
2023-05-02 17:41:06 +0530

    Update BoundedJointLearning.py

--- BoundedJointLearning.py
+++ BoundedJointLearning.py
@@ -186,13 +186,12 @@ class RwlGNN:
         y = self.weight.clone().detach()
         y = y.to(self.device)
         y.requires_grad = True
-        boundl=torch.log(torch.sqrt(torch.tensor(self.d)) * torch.square(torch.norm(self.A() - self.A(self.w_old))))
-        self.bound_losses.append(boundl.item())
         if iter%20 == 0:
             loss_fro = args.alpha* torch.norm(self.L(y) - L_noise, p='fro')
-
             loss_smooth_feat =args.beta* self.feature_smoothing(self.A(y), features)
             bound_loss = self.bound**2  * torch.log(torch.sqrt(torch.tensor(self.d)) * torch.square(torch.norm(self.A() - self.A(self.w_old))))
+            boundl=torch.log(torch.sqrt(torch.tensor(self.d)) * torch.square(torch.norm(self.A() - self.A(self.w_old))))
+            self.bound_losses.append(boundl.item())
             print(f'Total loss = {loss_fro + loss_smooth_feat}, Bound loss = {bound_loss}')
 
         normalized_adj = self.normalize(y)
